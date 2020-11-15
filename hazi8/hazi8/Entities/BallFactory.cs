3740d9d9836e5b84bcc3216c6c744119d1c4b9de
Bence Zsolt Báder <bence.bader@uni-corvinus.hu>
2020-11-15 13:20:02 +0100

    gyár

---
+++ hazi8/hazi8/Entities/BallFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hazi8.Entities
+{
+   public  class BallFactory
+    {
+
+
+
+        public Ball CreateNew()
+        {
+            return new Ball();
+        }
+    }
+}
--- hazi8/hazi8/Form1.cs
+++ hazi8/hazi8/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using hazi8.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,69 @@ namespace hazi8
 {
     public partial class Form1 : Form
     {
+        private BallFactory _factory;
+        public BallFactory Factory
+        {
+            get { return _factory; }
+            set { _factory = value; }
+        }
+
+        Timer createTimet, conveyorTimer;
+        List<Ball> _balls = new List<Ball>();
+
         public Form1()
         {
             InitializeComponent();
+
+           
+            mainPanel.Width = this.Width;
+
+            createTimet.Interval = 3000;
+            createTimet.Start();
+            createTimet.Tick += CreateTimet_Tick;
+            conveyorTimer.Interval = 10;
+            conveyorTimer.Start();
+            conveyorTimer.Tick += ConveyorTimer_Tick;
+        }
+
+
+        private void ConveyorTimer_Tick(object sender, EventArgs e)
+        {
+            int max = 0;
+            foreach (var ball in _balls)
+            {
+                ball.MoveBall();
+
+
+
+                if (ball.Left > max)
+                {
+
+                    max = ball.Left;
+                }
+            }
+
+
+
+            if (max >= 1000)
+            {
+                var rightBall = _balls[0];
+                mainPanel.Controls.Remove(rightBall);
+                _balls.Remove(rightBall);
+            }
+        }
+
+        private void CreateTimet_Tick(object sender, EventArgs e)
+        {
+            var ball = Factory.CreateNew();
+            _balls.Add(ball);
+            ball.Left = -ball.Width;
+            mainPanel.Controls.Add(ball);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
