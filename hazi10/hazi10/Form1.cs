56e3eb5b99e7b828ae475e7f9ab3acec95bc83d9
Bence Zsolt Báder <bence.bader@uni-corvinus.hu>
2020-11-26 20:51:37 +0100

    fist gamers

--- hazi10/hazi10/Form1.cs
+++ hazi10/hazi10/Form1.cs
@@ -17,12 +17,27 @@ namespace hazi10
         GameController gc = new GameController();
         GameArea ga;
 
+
+        int populationSize = 100;
+        int nbrOfSteps = 10;
+        int nbrOfStepsIncrement = 10;
+        int generation = 1;
+
         public Form1()
         {
             InitializeComponent();
-
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
+
+
+            for (int i = 0; i < populationSize; i++)
+            {
+                gc.AddPlayer(nbrOfSteps);
+            }
+          
+            gc.Start(true);
+
+
         }
 
         private void Form1_Load(object sender, EventArgs e)
